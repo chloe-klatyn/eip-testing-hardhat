e8e797fd37d42bcb7b6a69e04c9de2dd36a5a258
thryec <thryec@gmail.com>
2022-01-10 17:33:00 +0800

    set up ERC721 with ERC2981 sample contract that deploys on local node

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,22 @@
 const hre = require('hardhat')
 
 async function main() {
-  const Greeter = await hre.ethers.getContractFactory('Greeter')
-  const greeter = await Greeter.deploy('Hello, Hardhat!')
+  const ERC721 = await hre.ethers.getContractFactory('ERC721ContractWideRoyalties')
+  const erc721 = await ERC721.deploy('ERC721Royalties', 'ERC721')
 
-  await greeter.deployed()
+  await erc721.deployed()
 
-  console.log('Greeter deployed to:', greeter.address)
+  console.log('ERC721 deployed to:', erc721.address)
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (err) {
+    console.log(err)
     process.exit(1)
-  })
+  }
+}
+
+runMain()
