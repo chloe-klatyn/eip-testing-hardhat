95dbc5fc38a788e604e14155a8fedb196b34b414
thryec <thryec@gmail.com>
2022-01-11 11:04:01 +0800

    successfully tested deployment

--- contracts/ERC721ContractWideRoyalties.sol
+++ contracts/ERC721ContractWideRoyalties.sol
@@ -3,57 +3,47 @@
 
 pragma solidity ^0.8.4;
 
-import "hardhat/console.sol";
-import "./ERC2981.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import 'hardhat/console.sol';
+import './ERC2981.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract ERC721ContractWideRoyalties is ERC721, ERC2981, Ownable {
-    uint256 nextTokenId;
+  uint nextTokenId;
 
-    constructor(string memory name_, string memory symbol_)
-        ERC721(name_, symbol_)
-    {
-        console.log("owner of contract: ", msg.sender);
+  constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
+
+  /// @inheritdoc	ERC165
+  function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC2981) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
+
+  /// @notice Allows to set the royalties on the contract
+  /// @dev This function in a real contract should be protected with a onlyOwner (or equivalent) modifier
+  /// @param recipient the royalties recipient
+  /// @param value royalties value (between 0 and 10000)
+  function setRoyalties(address recipient, uint value) public {
+    _setRoyalties(recipient, value);
+  }
+
+  /// @notice Mint one token to `to`
+  /// @param to the recipient of the token
+  function mint(address to) external {
+    uint tokenId = nextTokenId;
+    _safeMint(to, tokenId, '');
+
+    nextTokenId = tokenId + 1;
+  }
+
+  /// @notice Mint several tokens at once
+  /// @param recipients an array of recipients for each token
+  function mintBatch(address[] memory recipients) external {
+    uint tokenId = nextTokenId;
+    for (uint i; i < recipients.length; i++) {
+      _safeMint(recipients[i], tokenId, '');
+      tokenId++;
     }
 
-    /// @inheritdoc	ERC165
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(ERC721, ERC2981)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
-
-    /// @notice Allows to set the royalties on the contract
-    /// @dev This function in a real contract should be protected with a onlyOwner (or equivalent) modifier
-    /// @param recipient the royalties recipient
-    /// @param value royalties value (between 0 and 10000)
-    function setRoyalties(address recipient, uint256 value) public {
-        _setRoyalties(recipient, value);
-    }
-
-    /// @notice Mint one token to `to`
-    /// @param to the recipient of the token
-    function mint(address to) external {
-        uint256 tokenId = nextTokenId;
-        _safeMint(to, tokenId, "");
-
-        nextTokenId = tokenId + 1;
-    }
-
-    /// @notice Mint several tokens at once
-    /// @param recipients an array of recipients for each token
-    function mintBatch(address[] memory recipients) external {
-        uint256 tokenId = nextTokenId;
-        for (uint256 i; i < recipients.length; i++) {
-            _safeMint(recipients[i], tokenId, "");
-            tokenId++;
-        }
-
-        nextTokenId = tokenId;
-    }
+    nextTokenId = tokenId;
+  }
 }
--- test/erc721.test.js
+++ test/erc721.test.js
@@ -1,7 +1,7 @@
 const { ethers } = require('hardhat')
 const { expect } = require('chai')
 
-describe('ERC721WithRoyalties', function () {
+describe('ERC721WithRoyalties', () => {
   let erc721
   beforeEach(async () => {
     ;[contractOwner, seller1, seller2, buyer1, buyer2] = await ethers.getSigners()
@@ -10,10 +10,24 @@ describe('ERC721WithRoyalties', function () {
     erc721 = await ERC721.deploy('ERC721Royalties', 'ERC721')
     await erc721.deployed()
     const contractAddress = erc721.address
-    console.log('nft deployed to: ', contractAddress)
   })
 
-  it('Should set the owner of the contract as contractOwner', async function () {
-    expect(await erc721.owner()).to.equal(contractOwner.address)
+  describe('Deployment', () => {
+    it('has a name', async () => {
+      expect(await erc721.name()).to.equal('ERC721Royalties')
+    })
+
+    it('has a symbol', async () => {
+      expect(await erc721.symbol()).to.equal('ERC721')
+    })
+
+    it('Should set the owner of the contract as contractOwner', async () => {
+      expect(await erc721.owner()).to.equal(contractOwner.address)
+    })
   })
+
+  // describe('Minting', async () => {
+  //   it('mints the correct amount of tokens to seller', async () => {})
+  //   it('transfers nft to buyer when purchase is made', async () => {})
+  // })
 })
