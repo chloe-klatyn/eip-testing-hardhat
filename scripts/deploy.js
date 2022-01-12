584d2edbffd056ca7df6f824ccb620eca424e458
thryec <thryec@gmail.com>
2022-01-12 13:55:55 +0800

    updated deploy script for erc1363

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,21 @@
 const hre = require('hardhat')
 
 async function main() {
-  const Greeter = await hre.ethers.getContractFactory('Greeter')
-  const greeter = await Greeter.deploy('Hello, Hardhat!')
+  const ERC1363 = await hre.ethers.getContractFactory('ERC1363')
+  const erc1363 = await ERC1363.deploy('ERC1363Token', 'ERC1363')
+  await erc1363.deployed()
 
-  await greeter.deployed()
-
-  console.log('Greeter deployed to:', greeter.address)
+  console.log('ERC1363 token deployed to:', erc1363.address)
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error)
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (err) {
+    console.log(err)
     process.exit(1)
-  })
+  }
+}
+
+runMain()
