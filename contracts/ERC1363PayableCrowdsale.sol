bdf4e37384b503c02103866f7efd85cac79d4bf0
thryec <thryec@gmail.com>
2022-01-12 15:51:01 +0800

    added crowdsale token contract as token to distribute

--- contracts/ERC1363PayableCrowdsale.sol
+++ contracts/ERC1363PayableCrowdsale.sol
@@ -1,5 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 // Adapted from https://github.com/vittominacori/erc1363-payable-token/blob/master/contracts/examples/ERC1363PayableCrowdsale.sol
+
 pragma solidity ^0.8.0;
 
 import 'hardhat/console.sol';
--- test/erc1363.test.js
+++ test/erc1363.test.js
@@ -3,14 +3,35 @@ const { ethers } = require('hardhat')
 
 describe('ERC1363', function () {
   beforeEach(async () => {
-    ;[contractOwner, seller1, seller2, buyer1, buyer2] = await ethers.getSigners()
+    ;[contractsOwner, payableWallet] = await ethers.getSigners()
 
-    const ERC1363 = await hre.ethers.getContractFactory('ERC1363')
+    const ERC1363 = await hre.ethers.getContractFactory('ERC1363Mock')
     const erc1363 = await ERC1363.deploy('ERC1363Token', 'ERC1363')
     await erc1363.deployed()
+
+    const CrowdsaleToken = await hre.ethers.getContractFactory('CrowdsaleToken')
+    const crowdsaleToken = await CrowdsaleToken.deploy()
+    await crowdsaleToken.deploy()
+
+    const ERC1363Payable = await hre.ethers.getContractFactory('ERC1363PayableCrowdsale')
+    const erc1363Payable = await ERC1363Payable.deploy(10, payableWallet.address, crowdsaleToken.address, erc1363.address)
+    await erc1363Payable.deployed()
+  })
+  describe('CrowdsaleToken Deployment', () => {
+    it('has a name', async () => {
+      expect(await crowdsaleToken.name()).to.equal('CrowdsaleToken')
+    })
+
+    it('has a symbol', async () => {
+      expect(await crowdsaleToken.symbol()).to.equal('CRWD')
+    })
+
+    it('Should set the owner of the contract as contractOwner', async () => {
+      expect(await crowdsaleToken.owner()).to.equal(contractOwner.address)
+    })
   })
 
-  describe('Deployment', () => {
+  describe('ERC1363 Deployment', () => {
     it('has a name', async () => {
       expect(await erc1363.name()).to.equal('ERC1363Token')
     })
